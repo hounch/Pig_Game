29c5cd6bd00591cc6ed5d2f1b24545d62b92802e
hounch <zenia20043lee@gmail.com>
2023-10-21 17:04:31 +0300

    js added

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let currentPlayer = 1;
 let playerNumbers = [];
-let player1score = 0;
-let player2score = 0;
+let player_score = Array();
+player_score[1] = 0;
+player_score[2] = 0;
 
 function arrSum(arr) {
   var sum = 0;
@@ -21,38 +22,64 @@ $(".roll").click(function () {
       playerNumbers.push(randomNumber);
       console.log(currentPlayer);
       $(".current-score1").html(arrSum(playerNumbers)); //
+      // player1score += arrSum(playerNumbers); //
     } else {
-      $(".1").html(arrSum(playerNumbers)); //
+      $(".current-score1").html("0"); //
       playerNumbers = [];
+      switchPlayer();
     }
   } else {
     if (randomNumber !== 1) {
       playerNumbers.push(randomNumber);
       console.log(currentPlayer);
       $(".current-score2").html(arrSum(playerNumbers)); //
+      // player2score += arrSum(playerNumbers); //
     } else {
-      $(".2").html(arrSum(playerNumbers)); //
+      $(".current-score2").html("0"); //
       playerNumbers = [];
+      switchPlayer();
     }
   }
 });
 
 function switchPlayer() {
-  $(`.${currentPlayer}`).html(arrSum(playerNumbers));
+  player_score[currentPlayer] += arrSum(playerNumbers);
+  $(`.${currentPlayer}`).html(player_score[currentPlayer]); //
   currentPlayer = currentPlayer == 1 ? 2 : 1;
   console.log(currentPlayer, "current player");
   $(".current-score2").html("0");
+  $(".current-score1").html("0");
   $(`div.player1`).toggleClass("active_player");
   $(`div.player2`).toggleClass("active_player");
+  playerNumbers = [];
 }
 
 $(".hold").click(function () {
+  if (player_score[currentPlayer] + arrSum(playerNumbers) == 5) {
+    win();
+  }
   switchPlayer();
 });
 
 $(".again").click(function () {
   $(".current-score1").html("0");
+  $(".current-score2").html("0");
   $(".score").html("0");
+  player_score[1] = 0;
+  player_score[2] = 0;
   playerNumbers = [];
-  currentPlayer = 1;
+  currentPlayer = 2;
+  switchPlayer();
 });
+
+function win() {
+  if (currentPlayer === 1) {
+    $("div.player1").style.backgroundColor = "#2F2F2F";
+    $(".hold").setAttribute("disabled", "");
+    $(".roll").setAttribute("disabled", "");
+  } else {
+    $("div.player2").style.backgroundColor = "#2F2F2F";
+    $(".hold").setAttribute("disabled", "");
+    $(".roll").setAttribute("disabled", "");
+  }
+}
