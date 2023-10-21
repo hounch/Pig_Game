677111f57596787759ea3b19eaf26647f23a0a4e
hounch <zenia20043lee@gmail.com>
2023-10-21 17:39:16 +0300

    game finished

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-let currentPlayer = 1;
-let playerNumbers = [];
-let player_score = Array();
-player_score[1] = 0;
-player_score[2] = 0;
+let currentPlayer, playerNumbers, player_score;
+
+const init = function () {
+  currentPlayer = 1;
+  playerNumbers = [];
+  player_score = Array();
+  player_score[1] = 0;
+  player_score[2] = 0;
+  $(".current-score1").html("0");
+  $(".current-score2").html("0");
+  $(".score").html("0");
+  $("div.player1").addClass("active_player");
+  $("div.player1").removeClass("winner");
+  $("div.player2").removeClass("winner active_player");
+};
+
+init();
+
+$(".again").click(function () {
+  init();
+});
 
 function arrSum(arr) {
   var sum = 0;
@@ -20,9 +36,7 @@ $(".roll").click(function () {
   if (currentPlayer === 1) {
     if (randomNumber !== 1) {
       playerNumbers.push(randomNumber);
-      console.log(currentPlayer);
-      $(".current-score1").html(arrSum(playerNumbers)); //
-      // player1score += arrSum(playerNumbers); //
+      $(".current-score1").html(arrSum(playerNumbers));
     } else {
       $(".current-score1").html("0"); //
       playerNumbers = [];
@@ -31,9 +45,7 @@ $(".roll").click(function () {
   } else {
     if (randomNumber !== 1) {
       playerNumbers.push(randomNumber);
-      console.log(currentPlayer);
-      $(".current-score2").html(arrSum(playerNumbers)); //
-      // player2score += arrSum(playerNumbers); //
+      $(".current-score2").html(arrSum(playerNumbers));
     } else {
       $(".current-score2").html("0"); //
       playerNumbers = [];
@@ -46,7 +58,6 @@ function switchPlayer() {
   player_score[currentPlayer] += arrSum(playerNumbers);
   $(`.${currentPlayer}`).html(player_score[currentPlayer]); //
   currentPlayer = currentPlayer == 1 ? 2 : 1;
-  console.log(currentPlayer, "current player");
   $(".current-score2").html("0");
   $(".current-score1").html("0");
   $(`div.player1`).toggleClass("active_player");
@@ -55,31 +66,20 @@ function switchPlayer() {
 }
 
 $(".hold").click(function () {
-  if (player_score[currentPlayer] + arrSum(playerNumbers) == 5) {
+  if (player_score[currentPlayer] + arrSum(playerNumbers) >= 100) {
     win();
   }
   switchPlayer();
 });
 
-$(".again").click(function () {
-  $(".current-score1").html("0");
-  $(".current-score2").html("0");
-  $(".score").html("0");
-  player_score[1] = 0;
-  player_score[2] = 0;
-  playerNumbers = [];
-  currentPlayer = 2;
-  switchPlayer();
-});
-
 function win() {
   if (currentPlayer === 1) {
-    $("div.player1").style.backgroundColor = "#2F2F2F";
-    $(".hold").setAttribute("disabled", "");
-    $(".roll").setAttribute("disabled", "");
+    $("div.player1").addClass("winner");
+    $(".hold").prop("disabled", true);
+    $(".roll").prop("disabled", true);
   } else {
-    $("div.player2").style.backgroundColor = "#2F2F2F";
-    $(".hold").setAttribute("disabled", "");
-    $(".roll").setAttribute("disabled", "");
+    $("div.player2").addClass("winner");
+    $(".hold").prop("disabled", true);
+    $(".roll").prop("disabled", true);
   }
 }
